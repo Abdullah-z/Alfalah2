af858edb926637f48bd993fa4abf4a965d1c0d28
Abdullah Zafar <abdullahzafar994@gmail.com>
2023-03-16 13:08:35 +0500

    UI Changes + Other Features added

--- src/components/CollapseFAQ.tsx
+++ src/components/CollapseFAQ.tsx
@@ -1,28 +1,45 @@
-import {View} from 'react-native';
+import {TouchableWithoutFeedback, View} from 'react-native';
 
 import React, {useState} from 'react';
 import Collapsible from 'react-native-collapsible';
 import {useTheme} from '../hooks';
 import Text from './Text';
 import Block from './Block';
+import {Icon, Image} from 'native-base';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function CollapseFAQ(props) {
   const [isCollapsed, setisCollapsed] = useState(true);
   const {assets, colors, fonts, gradients, sizes} = useTheme();
   return (
-    <Block primary marginBottom={sizes.xs} marginHorizontal={sizes.xs}>
-      <Text
-        padding={sizes.sm}
-        h5
-        white
+    <Block marginHorizontal={sizes.xs}>
+      <TouchableWithoutFeedback
         onPress={() => {
           isCollapsed ? setisCollapsed(false) : setisCollapsed(true);
         }}>
-        {props.heading}
-      </Text>
+        <Block primary row marginBottom={sizes.xs}>
+          <View width={'80%'}>
+            <Text padding={sizes.sm} marginBottom={sizes.sm} white h5 semibold>
+              {props.heading}
+            </Text>
+          </View>
+          <Block align="flex-end" justify="center">
+            <Icon
+              mb="1"
+              as={
+                <Ionicons name={isCollapsed ? 'chevron-down' : 'chevron-up'} />
+              }
+              size="xl"
+              color={colors.white}
+            />
+          </Block>
+        </Block>
+      </TouchableWithoutFeedback>
       <Collapsible collapsed={isCollapsed}>
         <Block white>
-          <Text black>{props.detail}</Text>
+          <Text black padding={sizes.sm}>
+            {props.detail}
+          </Text>
         </Block>
       </Collapsible>
     </Block>
